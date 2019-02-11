5dd8c3e2da849603fba4b4fb4e75610019b7dabb
Xliying <cly184114@163.com>
2019-02-11 14:34:08 +0800

    init

---
+++ Arithmetic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Arithmetic
+//
+//  Created by 谢立颖 on 2019/2/11.
+//  Copyright © 2019 xiely. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
